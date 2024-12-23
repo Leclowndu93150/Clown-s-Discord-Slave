a95c4b3e56860c2d701d0cc33bc60f65d2364e4d
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-12-23 22:51:05 +0100

    real

--- features/AmongusVoice.py
+++ features/AmongusVoice.py
@@ -1,6 +1,5 @@
 import asyncio
 import json
-
 import websockets
 from discord.ext import commands
 
@@ -8,7 +7,7 @@ from discord.ext import commands
 class AmongUsVoice(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.voice_channel = None
+        self.channel_id = 1320854477464404090
         self.bot.loop.create_task(self.connect_capture())
 
     async def connect_capture(self):
@@ -19,36 +18,29 @@ class AmongUsVoice(commands.Cog):
             except:
                 await asyncio.sleep(5)
 
-    @commands.command()
-    async def setvoice(self, ctx):
-        if ctx.author.voice:
-            self.voice_channel = ctx.author.voice.channel
-            await ctx.send(f"Set channel: {self.voice_channel.name}")
-
     async def handle_states(self, ws):
         while True:
             data = json.loads(await ws.recv())
             if data['EventID'] == 0:
                 state = json.loads(data['EventData'])['NewState']
-                if self.voice_channel:
+                channel = self.bot.get_channel(self.channel_id)
+                if channel:
                     if state == 2:
-                        await self.unmute_all()
+                        await self.unmute_all(channel)
                     elif state == 1:
-                        await self.mute_all()
+                        await self.mute_all(channel)
             await asyncio.sleep(2)
 
-    async def mute_all(self):
-        if self.voice_channel:
-            for member in self.voice_channel.members:
-                try:
-                    await member.edit(mute=True)
-                except Exception as e:
-                    print(f"Failed to mute {member}: {e}")
+    async def mute_all(self, channel):
+        for member in channel.members:
+            try:
+                await member.edit(mute=True)
+            except Exception as e:
+                print(f"Failed to mute {member}: {e}")
 
-    async def unmute_all(self):
-        if self.voice_channel:
-            for member in self.voice_channel.members:
-                try:
-                    await member.edit(mute=False)
-                except Exception as e:
-                    print(f"Failed to unmute {member}: {e}")
+    async def unmute_all(self, channel):
+        for member in channel.members:
+            try:
+                await member.edit(mute=False)
+            except Exception as e:
+                print(f"Failed to unmute {member}: {e}")
